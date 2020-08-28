515c7c4d5a0818a85ba489de56bb154b280f3002
deaneturner <service@deaneturner.com>
2020-08-28 15:21:45 -0400

    tslint fixes

--- 23 - Using Reactive Extensions/End of Chapter/exampleApp/src/app/core/core.module.ts
+++ 23 - Using Reactive Extensions/End of Chapter/exampleApp/src/app/core/core.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
-import { ModelModule } from "../model/model.module";
-import { TableComponent } from "./table.component";
-import { FormComponent } from "./form.component";
-import { SharedState, SHARED_STATE } from "./sharedState.model";
-import { Subject } from "rxjs";
-import { StatePipe } from "./state.pipe";
-import { MessageModule } from "../messages/message.module";
-import { MessageService } from "../messages/message.service";
-import { Message } from "../messages/message.model";
-import { Model } from "../model/repository.model";
-import { MODES } from "./sharedState.model";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ModelModule } from '../model/model.module';
+import { TableComponent } from './table.component';
+import { FormComponent } from './form.component';
+import { SharedState, SHARED_STATE } from './sharedState.model';
+import { Subject } from 'rxjs';
+import { StatePipe } from './state.pipe';
+import { MessageModule } from '../messages/message.module';
+import { MessageService } from '../messages/message.service';
+import { Message } from '../messages/message.model';
+import { Model } from '../model/repository.model';
+import { MODES } from './sharedState.model';
 
 @NgModule({
     imports: [BrowserModule, FormsModule, ModelModule, MessageModule],
@@ -21,12 +21,13 @@ import { MODES } from "./sharedState.model";
         provide: SHARED_STATE,
         deps: [MessageService, Model],
         useFactory: (messageService, model) => {
-            let subject = new Subject<SharedState>();
+            const subject = new Subject<SharedState>();
             subject.subscribe(m => messageService.reportMessage(
+                // tslint:disable-next-line:triple-equals
                     new Message(MODES[m.mode] + (m.id != undefined
-                        ? ` ${model.getProduct(m.id).name}` : "")))
+                        ? ` ${model.getProduct(m.id).name}` : '')))
                 );
-            return subject;       
+            return subject;
         }
     }]
 })
