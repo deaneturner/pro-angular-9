133d6241a96cc2ac21091869ceb947445aa0f7b0
deaneturner <service@deaneturner.com>
2020-08-30 08:29:48 -0400

    baby steps

--- todo/src/app/todoList.ts
+++ todo/src/app/todoList.ts
@@ -1,6 +1,14 @@
 import { TodoItem } from './todoItem';
 
 export class TodoList {
-    constructor(public user: string, todoItems: TodoItem[] = []) {
+    constructor(public user: string, private todoItems: TodoItem[] = []) {
+    }
+
+    get items(): TodoItem[] {
+        return this.todoItems;
+    }
+
+    addItem(item: string) {
+        this.todoItems.push(new TodoItem(item));
     }
 }
