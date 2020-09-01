b727fd2eb16a19786441b13d90bb8d4c9ea1d3af
deaneturner <service@deaneturner.com>
2020-09-01 12:36:20 -0400

    progression

--- SportsStoreScratch/src/app/model/product.repository.ts
+++ SportsStoreScratch/src/app/model/product.repository.ts
@@ -1,4 +1,18 @@
+import { StaticDataSource } from './static.datasource';
+import { Product } from './product.model';
+
 export class ProductRepository {
-  constructor() {
+  private products: Product[] = [];
+  private categories: string[] = [];
+
+  constructor(private dataSource: StaticDataSource) {
+  }
+
+  getProduct(id: number): Product {
+    return this.products.find((product) => id === product.id );
+  }
+
+  getCategories(): string[] {
+    return this.categories;
   }
 }
