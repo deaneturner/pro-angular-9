4f3f4671d4b7c589707e440b74953021aaeabd02
deaneturner <service@deaneturner.com>
2020-09-04 12:51:41 -0400

    checkout form

--- SportsStoreScratch/src/app/store/checkout-component/checkout-component.component.ts
+++ SportsStoreScratch/src/app/store/checkout-component/checkout-component.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from '../../model/order.model';
+import { NgForm } from '@angular/forms';
+import { OrderRepository } from '../../model/order.repository';
 
 @Component({
   selector: 'app-checkout-component',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout-component.component.css']
 })
 export class CheckoutComponent implements OnInit {
+  orderSent = false;
+  submitted = false;
 
-  constructor() { }
+  constructor(public order: Order, private orderRepository: OrderRepository) {
+  }
 
   ngOnInit(): void {
   }
 
+  submitOrder(form: NgForm) {
+    this.submitted = true;
+    if(form.valid) {
+      this.orderRepository.saveOrders(this.order)
+        .subscribe(() => {
+          this.order.clear();
+          this.submitted = false;
+          this.orderSent = true;
+        });
+    }
+    return true;
+  }
 }
