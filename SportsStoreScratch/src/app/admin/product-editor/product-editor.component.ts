0bf46213ef21bf9583d0d89244406e0a2104c0d9
deaneturner <service@deaneturner.com>
2020-09-07 13:15:43 -0400

    product edit create

--- SportsStoreScratch/src/app/admin/product-editor/product-editor.component.ts
+++ SportsStoreScratch/src/app/admin/product-editor/product-editor.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../../model/product.model';
+import { ProductRepository } from '../../model/product.repository';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-editor',
@@ -6,8 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-editor.component.css']
 })
 export class ProductEditorComponent implements OnInit {
+  editing = false;
+  product = new Product();
 
-  constructor() { }
+  constructor(private productRepository: ProductRepository, activeRoute: ActivatedRoute) {
+    this.editing = activeRoute.snapshot.params['mode'] === 'edit';
+    if (this.editing) {
+      Object.assign(this.product, this.productRepository.getProduct(activeRoute.snapshot.params['id']));
+    }
+  }
 
   ngOnInit(): void {
   }
--- SportsStoreScratch/src/app/admin/product-table/product-table.component.ts
+++ SportsStoreScratch/src/app/admin/product-table/product-table.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../../model/product.model';
+import { ProductRepository } from '../../model/product.repository';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-table',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-table.component.css']
 })
 export class ProductTableComponent implements OnInit {
+  editing = false;
+  product = new Product();
 
-  constructor() { }
+  constructor(private productRepository: ProductRepository, activeRoute: ActivatedRoute) {
+    this.editing = activeRoute.snapshot.params['mode'] === 'edit';
+
+  }
 
   ngOnInit(): void {
   }
 
+  getProducts(category?: string): Product[] {
+    return this.productRepository.getProducts(category);
+  }
+
+  deleteProduct(id: number) {
+    this.productRepository.deleteProduct(id);
+  }
 }
